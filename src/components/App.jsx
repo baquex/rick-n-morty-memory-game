4df5d2a130af3b3f964741c3967bde365fcc5f85
Miguel <miguel.cuevas18@gmail.com>
2022-04-03 17:22:23 -0500

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl font-bold'>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
