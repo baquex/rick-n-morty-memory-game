7bdca8b0ab535dd6f260ca6872a39a07755e42f0
Miguel <miguel.cuevas18@gmail.com>
2022-04-03 20:50:07 -0500

    landing page with input and cta

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import '../styles/App.css';
 
 function App() {
+
+  const [character, setCharacter] = useState('');
+
+  const getCharacters = async () => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${character}`);
+    const char = await response.json();
+    console.log(char.results);
+    setCharacter('');
+  }
+
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold'>Hola mundo</h1>
+    <div className="flex flex-col align-middle">
+      <input className='character-input' 
+            onChange={ e => setCharacter(e.target.value)} 
+            type='text' 
+            placeholder='Enter Character Name'
+            value={character} />
+      <button className='start-btn' onClick={ getCharacters }>
+        Start
+      </button>
     </div>
   );
 }
