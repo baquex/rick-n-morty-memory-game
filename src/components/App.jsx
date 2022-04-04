ad853181533cdaffd80e9f9873b0f140e3937dbf
Miguel <miguel.cuevas18@gmail.com>
2022-04-04 16:52:00 -0500

    card positioning mobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,51 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import Card from './card/Card'
 import '../styles/App.css';
 
 function App() {
 
   const [character, setCharacter] = useState('');
+  const [charArray, setCharrArray] = useState([]);
 
-  const getCharacters = async () => {
+  const getCharacters = useCallback( async () => {
     const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${character}`);
     const char = await response.json();
-    console.log(char.results);
+    setCharrArray(char.results);
     setCharacter('');
+  }, [charArray]);
+
+  const makeCards = () => {
+
   }
 
   return (
     <div className="flex flex-col align-middle">
-      <input className='character-input' 
-            onChange={ e => setCharacter(e.target.value)} 
-            type='text' 
-            placeholder='Enter Character Name'
-            value={character} />
-      <button className='start-btn' onClick={ getCharacters }>
-        Start
-      </button>
+      { /*charArray.length == 0 ?
+      <> 
+        <input className='character-input' 
+              onChange={ e => setCharacter(e.target.value)} 
+              type='text' 
+              placeholder='Enter Character Name'
+              value={character} />
+        <button className='start-btn' onClick={ getCharacters }>
+          Start
+        </button>
+      </>
+  : <Card /> */}
+      <div className="card-holder">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,7 @@
+export default function () {
+  return(
+    <div className="single-card">
+      <img className="w-full" src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" />
+    </div>
+  )
+}
