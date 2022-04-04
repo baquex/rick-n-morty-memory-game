03880fe4615affd078537460036c8da3b50fc68d
Miguel <miguel.cuevas18@gmail.com>
2022-04-04 17:29:49 -0500

    added flipping capability to cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import cardbg from '../../images/card-bg.png';
+
+
 export default function () {
+
+  const [url, setUrl] = useState(cardbg);
+  const imgURL = 'https://rickandmortyapi.com/api/character/avatar/2.jpeg';
+  
+  const flip = (e) => {
+    setUrl(imgURL);
+  }
+  
   return(
-    <div className="single-card">
-      <img className="w-full" src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" />
+    <div className="single-card" onClick={ (e)=> flip(e)}>
+      <img className="w-full" src={url} />
     </div>
   )
 }
