cb8f9a660255b1645bc2363b3c929ae39c00985e
Miguel <miguel.cuevas18@gmail.com>
2022-04-04 20:50:23 -0500

    added images to cards from user input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,45 +7,80 @@ function App() {
   const [character, setCharacter] = useState('');
   const [charArray, setCharrArray] = useState([]);
 
-  const getCharacters = useCallback( async () => {
+  //makes API call to get the character based on user input
+  const getCharacters = useCallback( async (character) => {
     const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${character}`);
     const char = await response.json();
-    setCharrArray(char.results);
+    let apiImgs = [];
+
+    //adds images to image array
+    for (let i = 0; i < 6; i++){
+      apiImgs.push(char.results[i].image)
+    }
+
+    //takes the image array to make the cards for the game
+    let cardComponentArray = makeCards(apiImgs);
+
+    setCharrArray(cardComponentArray);
     setCharacter('');
-  }, [charArray]);
+  }, []);
 
-  const makeCards = () => {
+  //receives an array and makes all cards
+  const makeCards = (apiImg) => {
+    let cardArray = [];
+    let imgArray = [];
+    let imgIdx = 0;
 
+    for (let i = 0; i < 12; i+=2){
+     imgArray[i] = {'src': apiImg[imgIdx], 'id': imgIdx};
+     imgArray[i+1] = {'src': apiImg[imgIdx], 'id': imgIdx};
+     imgIdx++;
+    }
+
+    //takes the 12 card array and shuffles them
+    shuffleArray(imgArray);
+
+    //maps the card image to each card
+    imgArray.map( (img, idx)=>{
+      cardArray.push(<Card key={idx} imgSrc={img.src} imgId={img.id} />)
+    } );
+
+    return cardArray;
   }
+
+  //generic shuffliing function
+  const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+
+        // Generate random number
+        var j = Math.floor(Math.random() * (i + 1));
+
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
 
   return (
     <div className="flex flex-col align-middle">
-      { /*charArray.length == 0 ?
+      { charArray.length === 0 ?
       <> 
         <input className='character-input' 
               onChange={ e => setCharacter(e.target.value)} 
               type='text' 
               placeholder='Enter Character Name'
               value={character} />
-        <button className='start-btn' onClick={ getCharacters }>
+        <button className='start-btn' onClick={ () => getCharacters(character) }>
           Start
         </button>
       </>
-  : <Card /> */}
+  : 
       <div className="card-holder">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        { charArray }
       </div>
+    }
     </div>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import cardbg from '../../images/card-bg.png';
 
-
-export default function () {
+export default function Card ({imgSrc, imgId}) {
 
   const [url, setUrl] = useState(cardbg);
-  const imgURL = 'https://rickandmortyapi.com/api/character/avatar/2.jpeg';
+  const imgURL = imgSrc;
   
-  const flip = (e) => {
+  const flip = () => {
     setUrl(imgURL);
   }
   
   return(
-    <div className="single-card" onClick={ (e)=> flip(e)}>
-      <img className="w-full" src={url} />
+    <div className="single-card" onClick={ ()=> flip()}>
+      <img className="w-full" src={url} alt="cardimg" />
     </div>
   )
 }
